f18f3a6a1759373ec90179990b6b36a54e76f7ff
Soroush Sahraei <soroush.sahraei@gmail.com>
2023-05-18 00:37:00 +0330

    Fix tester

--- Templates/generator/tester.sh.cpp
+++ Templates/generator/tester.sh.cpp
@@ -5,11 +5,11 @@ g++ -std=c++17 -o main main.cpp
 g++ -std=c++17 -o naive naive.cpp
 g++ -std=c++17 -o gen gen.cpp
 ((i = 1))
-while diff main.txt naive.txt
+while diff main.txt naive.txt -Bb
 do
   echo $i
   ((i++))
-  ./gen
+  ./gen > test.txt
   ./main < test.txt > main.txt
   ./naive < test.txt > naive.txt
 done
