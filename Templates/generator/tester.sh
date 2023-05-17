79d7e8099bdfe657128c78c737dc3ece5f1fb203
Soroush Sahraei <soroush.sahraei@gmail.com>
2023-05-18 00:32:18 +0330

    I forgor

--- Templates/generator/tester.sh
+++ Templates/generator/tester.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-touch main.txt
-touch naive.txt
+echo "" > main.txt
+echo "" > naive.txt
 g++ -std=c++17 -o main main.cpp
 g++ -std=c++17 -o naive naive.cpp
 g++ -std=c++17 -o gen gen.cpp
-(i = 1)
+((i = 1))
 while diff main.txt naive.txt
 do
   echo $i
@@ -13,5 +13,3 @@ do
   ./main < test.txt > main.txt
   ./naive < test.txt > naive.txt
 done
-rm main.txt
-rm naive.txt
