ddc76513a222d540b77f201c66b95ddb569901f3
Soroush Sahraei <soroush.sahraei@gmail.com>
2024-12-13 15:46:09 +0330

    Fix fenwick Add

--- DS/fenwick.cpp
+++ DS/fenwick.cpp
@@ -1,10 +1,10 @@
 void add(int pos,int x){
-    for(;pos<maxn;pos+=pos&(-pos))
+    for(pos+=5;pos<maxn;pos+=pos&(-pos))
         fen[pos]+=x;
 }
 int get(int pos){
     int ans = 0;
-    for(;pos;pos-=pos&(-pos))
+    for(pos+=5;pos;pos-=pos&(-pos))
         ans+=fen[pos];
     return(ans);
 }
